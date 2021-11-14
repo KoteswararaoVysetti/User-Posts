79c1fb7ed6008f06a9a46dc03f99ff400dd2ec2a
Im_a_Negative1 <vvkoteswararao123@gmail.com>
2021-11-14 15:16:37 +0530

    Done

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+ 
+const app = express();
+app.use(express.static(__dirname+"/FrontEnd"));
+app.get("/", function(req, res){
+    res.sendFile(__dirname+"/FrontEnd/users.html");
+})
+
+// Heroku will automatically set an environment variable called PORT
+const PORT = process.env.PORT || 3000;
+ 
+// Start the server
+app.listen(PORT, function(){
+    console.log("Server Starting running on http://localhost:"+PORT);
+})
